25d171f34587f6d1a60ee76afdabaf8425ed5908
stanboymusic <adrcproducciones@gmail.com>
2026-01-10 20:09:35 -0400

    actualizaciones varias3

--- nex-admin/middleware.ts
+++ nex-admin/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { verifyToken } from './lib/jwt'
+import { verifyToken } from '@/lib/jwt'
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get('token')?.value
