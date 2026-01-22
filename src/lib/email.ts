05cf9df0bd44061fb769047403f963de04a96cbf
stanboymusic <adrcproducciones@gmail.com>
2026-01-22 08:15:51 -0400

    fix: correct nodemailer createTransport function name

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
   auth: {
